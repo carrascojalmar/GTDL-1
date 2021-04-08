66b3395b093e93d488833f6abf5ae70110f7bb84
Jalmar M F Carrasco <carrascojalmar@gmail.com>
2021-04-08 16:00:39 -0300

    Update resiGTDL.R

--- R/resiGTDL.R
+++ R/resiGTDL.R
@@ -34,7 +34,7 @@ quantile.GTDL <- function(t,formula,param,censura){
   for(i in 1:length(t)){
     gamma.aux <-  x[i,]%*%matrix(param[3:(p+2)])
     param.aux <- c(param[1:2],gamma.aux)
-    conf[i] <- dGTDL(t=t[i],param=param.aux)  
+    conf[i] <- reability.GTDL(t=t[i],param=param.aux)  
   }
   
   qr <- qnorm(censura* (1 - conf) + (1-censura)*runif(length(t),1-conf))
